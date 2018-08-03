245ca052e872d8fff384d707546d7da0fb2932b2
Gamerein <reinrijn@gmail.com>
2018-08-03 22:24:58 +0200

    improved help

--- src/commands/general/help.js
+++ src/commands/general/help.js
@@ -15,13 +15,28 @@ class Help {
         }
     }
     async run(Kirito, args, message, alias, prefix, chn) {
-        if (Kirito.commands[args[0]] || Kirito.commandAliases[args[0]])
-            chn.send(Kirito.renderHelp(args[0]));
-        else chn.send(
+        if(args[0]){
+            if (Kirito.commands[args[0]] || Kirito.commandAliases[args[0]])
+                message.respond(Kirito.renderHelp(args[0]));
+            else {
+                const StringSimilarity = require('string-similarity');
+                let ratings = StringSimilarity.findBestMatch(args[0], Object.keys(Kirito.commands)).ratings;
+                let similiarCommands = [];
+                for (let i = 0; i < ratings.length; i++) {
+                    const result = ratings[i];
+                    if (result.rating > 0.3)
+                        similiarCommands.push(result.target)
+                }
+                if (similiarCommands.length)
+                    message.respond(`Command \`${args[0]}\` not found.\n Similar commands: \`${similiarCommands.join(', ')}\``);
+                else message.respond(`Command \`${args[0]}\` not found.'`)
+            }
+        }
+        else message.respond(
         Kirito.config.admins.includes(message.author.id) ?
             Kirito.renderHelp() :
             Kirito.renderHelp().withoutAdmin()
-        )
+        );
     }
 }
 
