303062ed970cb8b6284abec7a34ab8ec1723d0c6
Rein F <public@reinfernhout.xyz>
2019-04-17 23:50:15 +0200

    wh open rework

--- src/commands/fun/wormhole.js
+++ src/commands/fun/wormhole.js
@@ -3,7 +3,7 @@ class Wormhole {
         this.help = {
             "description": `
 Send messages through time and space.
-Who knows where they might end???
+Who knows where they might end up???
 
 To send wormhole messages:
 [prefix]wh send <message>
@@ -65,72 +65,117 @@ To stop receiving wormhole messages:
 
         //Open
         else if (args[0] === "open") {
-            if (message.member.hasPermission("MANAGE_GUILD")) {
-                let channel = args[1];
-                if (!args[1])
-                    channel = `<#${message.channel.id}>`;
-                if (Kirito.channels.get(channel.replace(/<|#|>/g,""))) {
-                    let embed = new Kirito.Discord.RichEmbed()
-                    .setTitle("Confirmation opening wormhole")
-                    .setColor(0x0066ff)
-                    .setDescription(`Are you sure you want to open a wormhole in ${channel}?\n`
-                    + "This will allow messages from other guilds to come in")
+            if (!message.member.hasPermission("MANAGE_GUILD"))
+                return message.respond("You don\'t have the manage_server permission!")
+            
+            let channelID;
 
-                    let msg = await message.respond(embed);
-                    msg.confirm();
-                    msg.accept(() => {
-                        let guild = Kirito.guilds_.get(message.guild.id);
+            //Assume message channel if no channel specified
+            if (!args[1])
+                channelID = message.channel.id;
+            else
+                channelID = args[1].replace(/<|#|>/g,"");
 
-                        //If existing wormhole, close message
-                        if (message.guild.channels.has(guild.wormholeChannel))
-                            message.guild.channels.get(guild.wormholeChannel).send({
-                                embed:{
-                                    title:"The wormhole here has been closed!",
-                                    color: 0x0066ff
-                            }}
-                        )
-                        guild.wormholeChannel = channel.replace(/<|#|>/g,"");
-                        Kirito.guilds_.set(guild.id,guild);
-                        delete embed.description;
-                        embed.setTitle("Confirmed opening wormhole");
-                        msg.edit(embed);
-                        Kirito.channels.get(channel.replace(/<|#|>/g,"")).send({
-                            embed:{
-                                title:"A wormhole has been opened here!",
-                                color: 0x0066ff
-                        }});
-                    });
-                    msg.deny(() => {
-                        delete embed.description;
-                        embed.setTitle("Cancelled opening wormhole")
-                        msg.channel.send(embed)
-                    });
+            const channel = Kirito.channels.get(channelID);
 
-                } else message.respond("That is not a channel!");
+            if (!channel)
+                return  message.respond("That is not a channel!");
+            
+            if (!message.guild.me.permissionsIn(channel).has("SEND_MESSAGES"))
+                return  message.respond(`I can't open a wormhole in <#${channel.id}> because I don't have the permission to speak there.`);
+
+            const guild = Kirito.guilds_.get(message.guild.id);
+
+            let embed = new Kirito.Discord.RichEmbed().setColor(0x0066ff);
+
+            //There is already a wormhole open
+            if (message.guild.channels.has(guild.wormholeChannel)) {
+                
+                //Trying to open already open wormhole
+                if (guild.wormholeChannel == channel.id)
+                    return message.respond(`There is already a wormhole in <#${channel.id}>`);
+                
+                //Moving wormhole
+                else if (Kirito.channels.has(guild.wormholeChannel)) {
+
+                    let {name} = Kirito.channels.get(guild.wormholeChannel);
+
+                    embed
+                        .setTitle(`Confirmation moving wormhole from **${name}** to **${channel.name}**`)
+                        .setDescription(`There is already an existing wormhole at <#${guild.wormholeChannel}>.\n` + 
+                        `Are you sure you want to open a wormhole in <#${channel.id}> and close the existing wormhole at <#${guild.wormholeChannel}>?\n` +
+                        "This will allow messages from other guilds to come in.");
+                }
+
+
+            } else {
+                embed
+                    .setTitle(`Confirmation opening wormhole in **${channel.name}**`)
+                    .setDescription(`Are you sure you want to open a wormhole in <#${channel.id}>?\n`
+                    + "This will allow messages from other guilds to come in.");
             }
-            else message.respond("You don\'t have the manage_server permission!")
+
+            let msg = await message.respond(embed);
+            msg.confirm();
+
+            const moved = Kirito.channels.has(guild.wormholeChannel);
+
+            msg.accept(() => {
+                
+                if (moved)
+                Kirito.channels.get(guild.wormholeChannel).send({
+                    embed:{
+                        title:"The wormhole has been closed here!",
+                        color: 0x0066ff
+                }});
+
+                guild.wormholeChannel = channel.id;
+                Kirito.guilds_.set(guild.id,guild);
+                delete embed.description;
+                embed.setTitle(`Confirmed ${moved ? "moving" : "opening"} wormhole`);
+                msg.edit(embed);
+                Kirito.channels.get(channel.id).send({
+                    embed:{
+                        title:"A wormhole has been opened here!",
+                        color: 0x0066ff
+                }});
+
+            });
+            msg.deny(() => {
+                delete embed.description;
+                embed.setTitle(`Cancelled ${moved ? "moving" : "opening"} wormhole`)
+                msg.channel.send(embed)
+            });
+
         }
 
         //Close
         else if (args[0] === "close") {
-            let guild = Kirito.guilds_.get(message.guild.id);
-            if (message.member.hasPermission("MANAGE_GUILD")) {
-                if (!Kirito.channels.has(guild.wormholeChannel))
-                    return message.respond("There is no wormhole open in this server!")
-                let msg = await message.respond(`Are you sure you want to close the wormhole in <#${guild.wormholeChannel}>`)
-                msg.confirm();
-                msg.accept(() => {
-                    msg.edit(`Wormhole closed in <#${guild.wormholeChannel}>`);
-                    Kirito.channels.get(guild.wormholeChannel).send({
-                        embed:{
-                            title:"The wormhole here has been closed!",
-                            color: 0x0066ff
-                    }});
-                    guild.wormholeChannel = null;
-                    Kirito.guilds_.set(guild.id, guild);
-                }).deny(() => msg.edit(`Cancelled closing Wormhole in in <#${guild.wormholeChannel}>`))
-            } else message.respond("You don\'t have the manage_server permission!")
+
+            const guild = Kirito.guilds_.get(message.guild.id);
+
+            if (!message.member.hasPermission("MANAGE_GUILD"))
+                return message.respond("You don\'t have the manage_server permission!");
+            
+            if (!Kirito.channels.has(guild.wormholeChannel))
+                return message.respond("There is no wormhole open in this server!");
+            
+            let msg = await message.respond(`Are you sure you want to close the wormhole in <#${guild.wormholeChannel}>`)
+            msg.confirm();
+            msg.accept(() => {
+                msg.edit(`Wormhole closed in <#${guild.wormholeChannel}>`);
+                Kirito.channels.get(guild.wormholeChannel).send({
+                    embed:{
+                        title:"The wormhole here has been closed!",
+                        color: 0x0066ff
+                }});
+                guild.wormholeChannel = null;
+                Kirito.guilds_.set(guild.id, guild);
+            }).deny(() => msg.edit(`Cancelled closing Wormhole in in <#${guild.wormholeChannel}>`))
+
         }
+
+        //Neither open, send or close...
         else message.respond('That is not a valid argument. Take a look at this commands help:',Kirito.renderHelp(this.name))
     }
 }
