b2a82d95afc86fffac79623bd7c7d91dc9e2b664
Rein F <public@reinfernhout.xyz>
2020-04-08 04:59:38 +0200

    8ball

---
+++ src/commands/general/8ball.js
@@ -0,0 +1,46 @@
+class Eightball {
+    constructor() {
+        this.help = {
+            "description": "Ask the magic 8 ball a question",
+            "usage": "[prefix]8ball Should I ban all other bots except Kirito?"
+        }
+        this.conf = {
+            "disabled": false,
+            "aliases": ["ask"],
+            "perms": [],
+            "guildOnly": false,
+            "ownerOnly": false,
+            "expectedArgs": "str",
+            "sameVC": false,
+            "nsfw": false,
+            "requires": []
+        }
+    }
+    async run(Kirito, args, message, alias, prefix, chn) {
+		const answers = [
+			"as I see it, yes",
+			"ask again later",
+			"don’t count on it",
+			"it is certain",
+			"it is decidedly so",
+			"most likely",
+			"my reply is no",
+			"my sources say no",
+			"signs point to yes",
+			"very doubtful",
+			"yes",
+			"yes, definitely",
+			"i would like to say yes, but I say no",
+			"possibly",
+			"obiously not",
+			"only you can tell"
+		]
+
+		const answer = answers[Math.floor(Math.random() * answers.length)+1]
+
+		message.respond(`:8ball:: ${answer}`)
+
+    }
+}
+
+module.exports = Eightball;
